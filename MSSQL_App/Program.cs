296645b86364f5f689abd0e3cfce88ab31deb08e
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-04-27 13:50:21 +0300

    Add MSSql and Oracle DBs.

---
+++ MSSQL_App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace MSSQL_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string        connetionString;
+            SqlConnection cnn;
+
+            connetionString = "Server=unit-1019\\sqlexpress;Database=BeaversLife;Trusted_Connection=True;" +
+                              "MultipleActiveResultSets=True";
+            cnn             = new SqlConnection(connetionString);
+            cnn.Open();
+            
+            SqlCommand    command;
+            SqlDataReader dataReader;
+
+            string sql;
+            string output = "";
+
+            sql        = "select Name from Animals";
+            command    = new SqlCommand(sql, cnn);
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                output += dataReader.GetValue(0) + "\n";
+            }
+            
+            cnn.Close();
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Oracle_App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+
+namespace Oracle_App
+{
+    class Program
+    {
+        // C:\app\lilia.shamsutdinova\product\18.0.0
+        static void Main(string[] args)
+        {
+            // TODO: insert password
+            OracleConnection con = new OracleConnection("User Id=system;Password=system;Data Source = localhost:1521/XEPDB1;");
+            con.Open();
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT \'Hello World!\' FROM dual";
+ 
+            OracleDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            Console.WriteLine(reader.GetString(0));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
