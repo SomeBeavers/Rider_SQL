806995f6d5294796719879a3b5dec80b6ca1a359
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-04-27 15:20:49 +0300

    Add MySql.

---
+++ MySql_App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace MySql_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = @"server=localhost;userid=root;password=root;database=employees";
+
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            Console.WriteLine($"MySQL version : {con.ServerVersion}");
+        }
+    }
+}
--- Oracle_App/Program.cs
+++ Oracle_App/Program.cs
@@ -9,16 +9,17 @@ namespace Oracle_App
         // C:\app\lilia.shamsutdinova\product\18.0.0
         static void Main(string[] args)
         {
-            // TODO: insert password
+            // TODO: insert password BeaversLife1 BeaversLifeDB
+            // mysql root root
             OracleConnection con = new OracleConnection("User Id=system;Password=system;Data Source = localhost:1521/XEPDB1;");
             con.Open();
             OracleCommand cmd = con.CreateCommand();
-            cmd.CommandText = "SELECT \'Hello World!\' FROM dual";
+            cmd.CommandText = @"SELECT 'Hello World!' FROM dual";
+            cmd.CommandText = "SELECT COUNT(*) FROM ORDER_ITEMS";
  
             OracleDataReader reader = cmd.ExecuteReader();
             reader.Read();
             Console.WriteLine(reader.GetString(0));
-            Console.WriteLine("Hello World!");
         }
     }
 }
