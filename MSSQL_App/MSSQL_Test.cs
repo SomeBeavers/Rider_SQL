8d7b588545affa25c7df80887b0f54173222aa2b
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-08-19 16:19:20 +0300

    Update MSSQL tests.

---
+++ MSSQL_App/MSSQL_Test.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+
+namespace MSSQL_App
+{
+    public class MSSQL_Test
+    {
+        public const int ConstId = 1;
+
+        public const            string ConstString  = "'a'";
+        private static readonly string MinField     = "=1";
+        public static           int    StaticId     = 1;
+        public static           string StaticString = "'test','test'";
+        public                  int    InstanceId   = 1;
+
+
+        private void Concatenation(string parameterTitle)
+        {
+            var min         = 1;
+            var id          = 1;
+            var localString = "'b'";
+
+            var sql1       = "select id from Animals where Age = " + 1;
+            var sql1_const = "select id from Animals where Age = 1";
+
+            var sql2       = "select id from Animals where Age = " + min + " and Name !=" + "'Prancer'";
+            var sql2_const = "select id from Animals where Age = 1 and Name !='Prancer'";
+
+            var sql3       = "select id from " + GetTableName() + " where id > 1";
+            var sql3_const = "select id from Animals where id > 1";
+
+            var sql4       = "select id from Animals " + "where id" + MinField;
+            var sql4_const = "select id from Animals where id=1";
+
+            var sql5 = "select d.Title as DrawbackTitle,"                     +
+                       " F.Title as FoodTitle from Drawbacks d "              +
+                       "inner join DrawbackFood DF on d.Id = DF.DrawbacksId " +
+                       "inner join Food F on DF.FoodsId = F.Id";
+            var sql5_const =
+                "select d.Title as DrawbackTitle, F.Title as FoodTitle from Drawbacks d inner join DrawbackFood DF on d.Id = DF.DrawbacksId inner join Food F on DF.FoodsId = F.Id";
+
+            var sql6 = "select d.Title as DrawbackTitle, F.Title as FoodTitle from Drawbacks d " +
+                       "inner join DrawbackFood DF on d.Id = DF.DrawbacksId "                    +
+                       "inner join Food F on DF.FoodsId = F.Id "                                 +
+                       "where d.Id >"                                                            +
+                       id                                                                        +
+                       " and F.id >="                                                            +
+                       id;
+            var sql6_const = @"
+                        select d.Title as DrawbackTitle, F.Title as FoodTitle from Drawbacks d 
+                        inner join DrawbackFood DF on d.Id = DF.DrawbacksId 
+                        inner join Food F on DF.FoodsId = F.Id
+                        where d.Id >1 and F.id >=1";
+
+            var sql7 = "select id from Animals " +
+                       "where id <= "            + id;
+            var sql7_const = "select id from Animals where id <= 1";
+
+            var sql8       = "select t " + "from Animals";
+            var sql8_const = "select t from Animals";
+
+            var sql9 = "select d.Title as DrawbackTitle, F.Title as FoodTitle from Drawbacks d " +
+                       "inner join DrawbackFood DF on d.Id = DF.DrawbacksId "                    +
+                       "inner join Food F on DF.FoodsId = F.Id "                                 +
+                       "where d.Id >"                                                            +
+                       InstanceId                                                                +
+                       " and "                                                                   +
+                       "t = "                                                                    +
+                       ConstId;
+
+            var sql10 = "insert into Drawbacks"      +
+                        "(Title, Consequence_Name) " +
+                        "values ("                   +
+                        parameterTitle               +
+                        ","                          +
+                        ConstString                  +
+                        ")";
+
+            var sql11 = "insert into Drawbacks"      +
+                        "(Title, Consequence_Name) " +
+                        "values ("                   +
+                        StaticString                 +
+                        ")";
+
+            var sql12 = "insert into Drawbacks"      +
+                        "(Title, Consequence_Name) " +
+                        "values ("                   +
+                        parameterTitle               +
+                        ","                          +
+                        ConstString                  +
+                        "),("                        +
+                        StaticString                 +
+                        ","                          +
+                        "'test'"                     +
+                        ")";
+            var sql13 = "update Animals set " +
+                        "Name ="              +
+                        localString           +
+                        " where Id="          +
+                        id;
+            var sql14 = "select top(5) Name from Animals " +
+                        "where IpAddress like "            +
+                        localString                        +
+                        " escape "                         +
+                        localString;
+            var sql15 = "select * from Jobs " +
+                        "where Title in ("    +
+                        localString           +
+                        ","                   +
+                        parameterTitle        +
+                        ")";
+            var sql16 = "Delete from AnimalClub " +
+                        "where ClubId="           +
+                        id;
+
+            var sql17 = "select id from " + parameterTitle + " where id > 1";
+        }
+
+        private void StringFormat()
+        {
+            var id           = 1;
+            var animalsTable = "Animals";
+
+            var sql1 = string.Format("select {0} from Animals where {0} = {1}",   ConstString, id);
+            var sql2 = string.Format("select id from Jobs where Salary = {0}{1}", 1,           2);
+            var sql3 = string.Format("select * from Clubs where id {0}",          "= 1");
+            var sql4 = string.Format("select * from Clubs {0}",                   "where id = 1");
+            var sql5 = string.Format("select Age from Animals where Id={0} ",     id);
+
+            var sql6 = string.Format("select j.Title from Jobs j "            +
+                                     "inner join Animals A on j.Id = A.JobId" +
+                                     " where j.Id>{0} or j.Id = {1}",
+                id, id);
+            var sql7 = string.Format(@"select * from Drawbacks where 
+            Title in ({0}, {1})",
+                StaticString, "name");
+            var sql8 = string.Format("select Name + Age as \"{0}\" from Animals where Id = {1}", "AllInfo", 1);
+
+            var stringUsedInFormat = "select * from Animals where Age between {0} and {1}";
+            var sql9               = string.Format(stringUsedInFormat, 1, 100);
+            var sql10 = string.Format("select * from Food f "                      +
+                                      "inner join Animals A on f.AnimalId = A.Id " +
+                                      "where f.Id<>{0} "                           +
+                                      "and f.Title Like {1}", id, "'B%'");
+        }
+
+        private void StringInterpolation(string parameterTitle)
+        {
+            var id   = 1;
+            var sql1 = $"select Age from Animals where id > {id} and Id < {id + 10}";
+            var sql2 = @$"select sum(Salary) as {parameterTitle} from Jobs ""renamed"" 
+inner join Animals A on renamed.Id = A.JobId";
+            var sql3 = @$"Delete from Jobs 
+where Id=
+      {id}
+      and Title like {"'%a'"}";
+            var sql4 = "select Name from Animals where" + $" id={GetId()}";
+        }
+
+        private void Verbatim(string parameterTitle)
+        {
+        }
+
+        private void Escape()
+        {
+            var sql1       = "select \"id\" from Animals";
+            var sql2       = @"select ""id"" as Id from Animals";
+            var sql3       = @"select * from dbo.""Animals""";
+            var columnName = "id";
+            var sql4       = $"select \"{columnName}\" from Animals";
+            var sql5       = @$"select ""{columnName}"" from Animals";
+        }
+
+        private void WithParameters()
+        {
+            using var connection = new SqlConnection(Constants.ConnectionString);
+
+            var sql1    = "select Title from Clubs where Id > @SmallId";
+            var command = new SqlCommand(sql1, connection);
+            command.Parameters.Add(new SqlParameter("@SmallId", 1));
+
+            var sql2       = "select Title from Clubs where Id > @SmallId";
+            var command2   = new SqlCommand(sql2, connection);
+            var parameter2 = new SqlParameter();
+            parameter2.ParameterName = "@WrongName"; // change to @SmallId
+            parameter2.Value         = 1;
+            command2.Parameters.Add(parameter2);
+
+            var sql3 =
+                "select a.Name, g.TheGrade from Grades g" +
+                " left join Animals a"                    +
+                " on g.AnimalId = a.Id"                   +
+                " where g.TheGrade > @MinGrade";
+            var cmd6 = new SqlCommand(sql3, connection);
+            cmd6.Parameters.Add("@MinGrade", SqlDbType.Decimal);
+            cmd6.Parameters["@MinGrade"].Value = 4.5;
+        }
+
+        #region Helpers
+
+        private static string GetTableName()
+        {
+            return "Animals";
+        }
+
+        public static int GetId()
+        {
+            return 1;
+        }
+
+        public static void Smoke_ExecuteQueries()
+        {
+            // replace SQl
+            var columnName = "id";
+            var sql        = $"select \"{columnName}\" from Animals";
+
+            using var connection = new SqlConnection(Constants.ConnectionString);
+
+            var command5 = new SqlCommand();
+            command5.Connection = connection;
+
+            command5.CommandText = sql;
+            command5.CommandType = CommandType.Text;
+            //
+            // var parameter5_in = new SqlParameter();
+            // parameter5_in.ParameterName = "@MinSalary";
+            // parameter5_in.Value         = 1;
+            // command5.Parameters.Add(parameter5_in);
+            // command5.Parameters.Add("@AllSalary", SqlDbType.Int).Direction = ParameterDirection.Output;
+            //
+
+            connection.Open();
+            // command5.ExecuteNonQuery();
+            // Console.WriteLine(Convert.ToInt32(command5.Parameters["@AllSalary"].Value));
+
+            using var reader = command5.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}", reader[0]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+
+            reader.Close();
+        }
+
+        #endregion
+    }
+}
---
+++ MSSQL_App/Playground.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+
+namespace MSSQL_App
+{
+    public class Playground
+    {
+        private void Test1()
+        {
+            var sql1 = @"--Add column GUID to each table--
+EXEC sp_MSforeachtable '
+if not exists (select * from sys.columns 
+				where object_id = object_id(''?'')
+				and name = ''diagram_id'')
+alter table ? ADD GUID uniqueidentifier NOT NULL DEFAULT NEWID()'
+
+--Create temporary table with Primary Keys (in case of different PK Columns in each table)--
+SELECT  i.name AS IndexName,
+        OBJECT_NAME(ic.OBJECT_ID) AS TableName,
+        COL_NAME(ic.OBJECT_ID,ic.column_id) AS ColumnName
+into #tempPK
+FROM    sys.indexes AS i INNER JOIN 
+        sys.index_columns AS ic ON  i.OBJECT_ID = ic.OBJECT_ID
+									AND i.index_id = ic.index_id
+WHERE   i.is_primary_key = 1
+
+--Create temporary table with Foreign Keys where referenced table column is Primary Key--
+SELECT	f.name AS ForeignKey, OBJECT_NAME(f.parent_object_id) AS TableName,
+		COL_NAME(fc.parent_object_id, fc.parent_column_id) AS ColumnName,
+		OBJECT_NAME (f.referenced_object_id) AS ReferenceTableName,
+		COL_NAME(fc.referenced_object_id, fc.referenced_column_id) AS ReferenceColumnName
+into #temp
+FROM	sys.foreign_keys AS f INNER JOIN 
+		sys.foreign_key_columns AS fc ON f.OBJECT_ID = fc.constraint_object_id
+		JOIN #tempPK as pk ON pk.TableName = OBJECT_NAME (f.referenced_object_id) 
+							AND COL_NAME(fc.referenced_object_id, fc.referenced_column_id) = pk.ColumnName
+--Loop through #temp--
+BEGIN
+declare 
+@tableParent varchar(max),
+@tableReferenced varchar(max),
+@columnValues varchar(max),
+@columnID varchar(max),
+@foreignKey varchar(max),
+@newColumnName varchar(max)
+
+DECLARE FKs CURSOR LOCAL FOR (select TableName, ReferenceTableName, ColumnName, ReferenceColumnName, ForeignKey from #temp)
+declare @sql nvarchar(max),
+@sql2 nvarchar(max),
+@sql4 nvarchar(max),
+@sql3 nvarchar(max),
+@sql6 nvarchar(max)
+
+OPEN FKs
+FETCH NEXT FROM FKs into @tableParent, @tableReferenced, @columnValues, @columnID, @foreignKey
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	set @newColumnName = @columnValues+'_'+@tableReferenced+'_'+@tableParent+'_GUID'
+
+	--Add column to Parent table (ex. Values_Table_1_Table_2_GUID)--
+	set @sql6 = 'ALTER TABLE '+@tableParent+'
+		ADD  '+@newColumnName +' uniqueidentifier NOT NULL DEFAULT NEWID()'
+
+	--Set values to Parent table column (ex. Values_Table_1_Table_2_GUID) equal to Referenced GUID column values--
+	set @sql4 = 'update '+@tableParent+' set '+@newColumnName+'='+@tableReferenced+'.GUID
+		from '+@tableReferenced+ ' join '+@tableParent+' on '+@tableParent+'.'
+		+@columnValues+'='+@tableReferenced+'.'+@columnID
+
+	--Add Foreign Key from GUID to Parent table column (ex. Values_Table_1_Table_2_GUID)--
+	set @sql = 'ALTER TABLE '+@tableReferenced+'
+		ADD CONSTRAINT '+@tableReferenced+@foreignKey+'_GUID UNIQUE (GUID)'
+
+	set @sql2 = 'ALTER TABLE '+@tableParent+'
+		ADD FOREIGN KEY ('+@newColumnName+')
+		REFERENCES '+@tableReferenced+'(GUID)'
+
+	--Drop Foreign Key from ID to Values--
+	set @sql3 = 'ALTER TABLE '+@tableParent+'
+		DROP CONSTRAINT '+@foreignKey
+
+	EXEC sp_executeSQL @sql6
+	EXEC sp_executeSQL @sql4
+	EXEC sp_executeSQL @sql
+	EXEC sp_executeSQL @sql2
+	EXEC sp_executeSQL @sql3
+
+    FETCH NEXT FROM FKs into @tableParent, @tableReferenced, @columnValues, @columnID, @foreignKey
+END
+
+CLOSE FKs
+DEALLOCATE FKs
+drop table #temp
+END
+
+--Drop Primary Key columns and set GUID column to be new Primary Key--
+EXEC sp_MSforeachtable '
+declare @sql nvarchar(max),
+@sql2 nvarchar(max)
+if not exists (select * from sys.columns 
+				where object_id = object_id(''?'')
+				and name = ''diagram_id'')
+BEGIN
+SELECT	@sql2 = ''alter table ? Drop column '' + ColumnName  + '';'',
+		@sql = ''ALTER TABLE ? DROP CONSTRAINT '' + IndexName + '';''
+			FROM #tempPK
+			WHERE OBJECT_ID(TableName) = OBJECT_ID(''?'');
+EXEC sp_executeSQL @sql
+EXEC sp_executeSQL @sql2
+alter table? ADD PRIMARY KEY(GUID)
+END
+'
+drop table #tempPK ";
+        }
+
+        private void Test2()
+        {
+            using var connection = new SqlConnection(Constants.ConnectionString);
+
+            var command3 = new SqlCommand();
+            command3.Connection  = connection;
+            command3.CommandText = "sp_executesql";
+            command3.CommandType = CommandType.StoredProcedure;
+            var parameter3 = new SqlParameter();
+            parameter3.ParameterName = "@sql";
+            parameter3.SqlDbType     = SqlDbType.NVarChar;
+            parameter3.Value         = "select * from Jobs where Salary > @salary";
+            var parameter3_1 = new SqlParameter();
+            parameter3_1.ParameterName = "@salary";
+            parameter3_1.Value         = 1;
+            var parameter3_2 = new SqlParameter();
+            parameter3_2.ParameterName = "@parameters";
+            parameter3_2.SqlDbType     = SqlDbType.NVarChar;
+            parameter3_2.Value         = "@salary int";
+            command3.Parameters.Add(parameter3);
+            command3.Parameters.Add(parameter3_2);
+            command3.Parameters.Add(parameter3_1);
+
+            var pizza4      = new SqlParameter("parameter", "Pizza");
+            var queryString = "select * from Food where Title = @parameter";
+        }
+
+        private void Test3()
+        {
+            using var connection = new SqlConnection(Constants.ConnectionString);
+
+            var command4 = new SqlCommand();
+            command4.Connection  = connection;
+            command4.CommandText = "GetSalarySum";
+            command4.CommandType = CommandType.StoredProcedure;
+
+            var parameter4_in = new SqlParameter();
+            parameter4_in.ParameterName = "@MinSalary";
+            parameter4_in.Value         = 1;
+            command4.Parameters.Add(parameter4_in);
+            command4.Parameters.Add("@AllSalary", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+
+            connection.Open();
+            command4.ExecuteNonQuery();
+            Console.WriteLine(Convert.ToInt32(command4.Parameters["@AllSalary"].Value));
+
+            var command5 = new SqlCommand();
+            command5.Connection  = connection;
+            command5.CommandText = "select @AllSalary = SUM(Salary) from Jobs where Salary >@MinSalary";
+            command5.CommandType = CommandType.Text;
+
+            var parameter5_in = new SqlParameter();
+            parameter5_in.ParameterName = "@MinSalary";
+            parameter5_in.Value         = 1;
+            command5.Parameters.Add(parameter5_in);
+            command5.Parameters.Add("@AllSalary", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+
+            connection.Open();
+            command5.ExecuteNonQuery();
+            Console.WriteLine(Convert.ToInt32(command5.Parameters["@AllSalary"].Value));
+        }
+
+        public static SqlDataAdapter CreateSqlDataAdapter(SqlConnection connection)
+        {
+            // Assumes that connection is a valid SqlConnection object
+            var adapter = new SqlDataAdapter();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+            // Create the commands.
+            adapter.SelectCommand = new SqlCommand(
+                "SELECT Title, Salary FROM Jobs", connection);
+            adapter.InsertCommand = new SqlCommand(
+                "INSERT INTO Jobs (Title, Salary) " +
+                "VALUES (@Title, @Salary)", connection);
+            adapter.UpdateCommand = new SqlCommand(
+                "UPDATE Jobs SET Title = @Title, Salary = @Salary " +
+                "WHERE Title = @oldTitle", connection);
+            adapter.DeleteCommand = new SqlCommand(
+                "DELETE FROM Jobs WHERE Title = @Title", connection);
+
+            // Create the parameters.
+            adapter.InsertCommand.Parameters.Add("@Title",
+                SqlDbType.VarChar, 40, "Title");
+            adapter.InsertCommand.Parameters.Add("@Salary",
+                SqlDbType.VarChar, 40, "Salary");
+
+            adapter.UpdateCommand.Parameters.Add("@Title",
+                SqlDbType.VarChar, 40, "Title");
+            adapter.UpdateCommand.Parameters.Add("@Salary",
+                SqlDbType.VarChar, 40, "Salary");
+            adapter.UpdateCommand.Parameters.Add("@oldTitle",
+                    SqlDbType.VarChar, 40, "Title").SourceVersion =
+                DataRowVersion.Original;
+
+            adapter.DeleteCommand.Parameters.Add("@Title",
+                    SqlDbType.Char, 5, "Title").SourceVersion =
+                DataRowVersion.Original;
+
+            return adapter;
+        }
+    }
+}
